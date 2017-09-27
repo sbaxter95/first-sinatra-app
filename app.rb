73fbc32f5872389b856038604fd0af8ac29a4ed1
sbaxter95 <sbaxter95@outlook.com>
2017-09-27 10:35:50 +0100

    updated and improved

--- app.rb
+++ app.rb
@@ -1,26 +1,35 @@
 require 'sinatra'
 require_relative 'ToDoManager.rb'
 
+#Index - all of the data
 get '/todos' do
 	ToDoManager.index
 end
 
+#Create - create a new todo
 post '/todos' do
-	todo = params['todo']
+	todo = params[:todo]
 	ToDoManager.create(todo)
 end
 
+#/todos?todo=new todo
+
+#Show - show a single record
 get '/todos/:id' do
 	id = params[:id].to_i
 	ToDoManager.show(id)
 end
 
+#Update - update a single record
 put '/todos/:id' do
 	id = params[:id].to_i
-	update = params['update']
+	update = params[:update]
 	ToDoManager.update(id, update)
 end
 
+#/todos/0?update=updated todo
+
+#Delete - delete a single record
 delete '/todos/:id' do
 	id = params[:id].to_i
 	ToDoManager.delete(id)
